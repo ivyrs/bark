5fe5acf41e212c7b62540a2127c5a931d0fc998d
ivy forever <ivy@ivy.rs>
2026-09-14 19:15:13 +0100

    init: setup project

---
+++ devenv.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+
+{
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rust-analyzer"
+      "rustfmt"
+    ];
+  };
+
+  packages = with pkgs; [
+    pkg-config
+
+    gtk4
+    gtk4-layer-shell
+
+    bacon
+    cargo-nextest
+    watchexec
+    just
+
+    nixfmt # for devenv formatting
+  ];
+
+  git-hooks.hooks = {
+    clippy.enable = true;
+    rustfmt.enable = true;
+  };
+
+  scripts.watch.exec = ''
+    bacon clippy
+  '';
+}

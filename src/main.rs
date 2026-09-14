730eec184167072cb1379c6364de0c07aa1c464c
ivy forever <ivy@ivy.rs>
2026-09-14 19:24:52 +0100

    feat: we have a window!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Label};
+
+fn build_ui(app: &Application) {
+    let text = Label::new(Some("woof"));
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("bark")
+        .default_height(100)
+        .default_width(400)
+        .child(&text)
+        .build();
+
+    window.present();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder().application_id("rs.ivy.bark").build();
+
+    app.connect_activate(build_ui);
+
+    app.run();
 }
